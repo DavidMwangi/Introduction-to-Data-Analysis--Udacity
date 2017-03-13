2d5beb6a54c0e99b0b3bdfd4da22315b9654f4e7
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-03-13 16:02:56 +0300

    Single open function

--- openfiles.py
+++ openfiles.py
@@ -11,43 +11,16 @@ enrollments_filename = 'data/enrollments.csv'
 #     enrollments.append(row)
 # f.close()
 
-with open(enrollments_filename, 'rb') as f:
-    reader = unicodecsv.DictReader(f)
-    enrollments = list(reader)
-    
-### Write code similar to the above to load the engagement
-### and submission data. The data is stored in files with
-### the given filenames. Then print the first row of each
-### table to make sure that your code works. You can use the
-### "Test Run" button to see the output of your code.
+def openfile(filename):
+    with open(filename, 'rb') as f:
+        reader = unicodecsv.DictReader(f)
+        return list(reader)
+
 
 engagement_filename = 'data/daily_engagement.csv'
 submissions_filename = 'data/project_submissions.csv'
     
-daily_engagement = []
+print(openfile(enrollments_filename))
+print(openfile(engagement_filename))
+print(openfile(submissions_filename))
 
-file_d = open(engagement_filename, "rb")
-
-reader = unicodecsv.DictReader(file_d)
-
-for row in reader:
-    
-    daily_engagement. append(row)
-    
-file_d.close()
-
-print(daily_engagement[0])
-    
-project_submissions = []
-
-file_p = open(submissions_filename,"rb")
-
-reader = unicodecsv.DictReader(file_p)
-
-for row in reader:
-    
-    project_submissions.append(row)
-    
-file_p.close()
-
-print(project_submissions[0])
